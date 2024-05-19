ceeed050bb1b59932cab0f6dec48169b27d6598f
aboba <aboba@boba.abo>
2024-05-20 01:00:12 +0300

    first change store

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,52 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    board: [
+      ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'],
+      ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
+      ['', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', ''],
+      ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
+      ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r']
+    ],
+    currentPlayer: 'white',
+    selectPiece: null,
+    gameOver: false,
+    historyMoves: []
   },
   getters: {
+    board: (state) => state.board,
+    currentPlayer: (state) => state.currentPlayer,
   },
   mutations: {
+    MOVE_PIECE(state, { from, to }) {
+      const piece = state.board[from.row][from.col]
+      state.board[from.row][from.col] = ''
+      state.board[to.row][to.col] = piece
+      state.historyMoves.push([...state.board])
+    },
+    SWITCH_PLAYER(state) {
+      state.currentPlayer = state.currentPlayer === 'black' ? 'white' : 'black'
+    }
   },
   actions: {
+    movePiece({ commit, state }, move) {
+      function isValidMove(board, from, to) {
+        if (board[from.row][from.col] !== '' && board[to.row][to.col] === '')
+          return true
+        return false
+      }
+
+      if (isValidMove(state.board, move.from, move.to)) {
+        commit('MOVE_PIECE', { from: move.from, to: move.to })
+        commit('SWITCH_PLAYER')
+      }
+      else {
+        console.log('Invalid move!')
+      }
+    }
   },
   modules: {
   }
